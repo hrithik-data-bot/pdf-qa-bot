ff53fb64dbc3df58125c1744283683a7c3f57405
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-25 22:14:52 +0530

    added files

---
+++ pdf_qa_bot/embeddings.py
@@ -0,0 +1,6 @@
+"""Utilities for creating embeddings and vector spaces"""
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+
