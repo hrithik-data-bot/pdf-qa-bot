52d685dae37817d40f1386e704f53bda4f28a131
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-25 08:34:24 +0000

    added module for PDF Utils

---
+++ pdf_qa_bot/pdf_utils.py
@@ -0,0 +1,17 @@
+"""utilities for reading and writing PDFs"""
+
+from PyPDF2 import Pdfreader
+
+class PDFUtils:
+    """class for PDF Utils"""
+
+    def __init__(self, pdf_path: str) -> None:
+        """init method for PDF Utils"""
+    
+        self.pdf_path = pdf_path
+
+
+    def read_pdf(self) -> 
+        """read PDF"""
+        pass
+    
