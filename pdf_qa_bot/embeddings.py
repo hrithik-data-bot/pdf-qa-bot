494d43d7c6297ad108af034e5bb48c54dfdb7a60
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-25 22:41:08 +0530

    added text splitter

--- pdf_qa_bot/embeddings.py
+++ pdf_qa_bot/embeddings.py
@@ -4,3 +4,13 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 
+
+def text_splitter(raw_text: str):
+    """text splitter"""
+
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=800,
+                                          chunk_overalp=200, length_function=len)
+    texts = text_splitter.split_text(raw_text)
+    return texts
+
+
