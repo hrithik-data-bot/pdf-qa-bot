7e59d66540fb3968e504b13271db867ec0ad5614
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-08-26 12:47:22 +0530

    added ask module

---
+++ pdf_qa_bot/main.py
@@ -0,0 +1,23 @@
+"""main module for query the PDF"""
+
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from embeddings import create_embeddings
+
+
+class Ask:
+    """main class for QA"""
+
+    def __init__(self, query: str) -> None:
+        """init method"""
+
+        self.query = query
+        self.chain = load_qa_chain(OpenAI(), chain_type="stuff")
+
+
+    def ask(self, texts: list) -> str:
+        """execute the query"""
+
+        docs = create_embeddings(texts).similarity_search(self.query)
+        answer = self.chain.run(input_documents=docs, question=self.query)
+        return answer
